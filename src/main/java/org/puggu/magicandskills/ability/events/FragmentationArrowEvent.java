d54fa54ee92c6c8d80e096f629420d2480a07854
PugguEx <71161000+Puggu-EX@users.noreply.github.com>
2024-06-21 08:01:26 -0700

    Actually Works :D

---
+++ src/main/java/org/puggu/magicandskills/ability/events/FragmentationArrowEvent.java
@@ -0,0 +1,29 @@
+package org.puggu.magicandskills.ability.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+public class FragmentationArrowEvent extends Event {
+    private static final HandlerList handlers = new HandlerList();
+    private final Player player;
+
+    public FragmentationArrowEvent(Player player) {
+        this.player = player;
+    }
+
+    public Player getPlayer(){
+        return this.player;
+    }
+
+    @NonNull
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
